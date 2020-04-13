f386f9128ad41161dca54796cbdf7a48ad203ee3
christianjensenv <43252300+christianjensenv@users.noreply.github.com>
2020-04-12 18:20:59 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,13 +6,20 @@ import { NavLink } from 'react-router-dom';
 class Landing extends React.Component {
   render() {
     const gridStyle = { height: '100vh' };
-    const buttonStyle = { color: '#000000', width: '200px', background: '#006400', marginBottom: '20px' };
+    const buttonStyle = {
+      color: '#000000',
+      width: '200px',
+      background: '#006400',
+      marginBottom: '20px',
+      fontSize: '25px',
+    };
+    const imageStyle = { marginBottom: '20px', size: 'medium' };
     return (
         <div className='study-uhp-landing-background'>
           <Grid style={gridStyle} verticalAlign='middle' textAlign='center' container>
             <Grid.Column centered>
               <Grid.Row>
-                <Image size='medium' src="/images/studyuhp_logo_square.png"/>
+                <Image centered style={imageStyle} src="/images/studyuhp_logo_square.png"/>
               </Grid.Row>
               <Grid.Row>
                 <Button animated style={buttonStyle} as={NavLink} exact to="/signup">
