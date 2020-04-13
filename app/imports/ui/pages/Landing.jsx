544cde576f917152b46232022ca988f71af05114
christianjensenv <43252300+christianjensenv@users.noreply.github.com>
2020-04-12 18:05:48 -1000

    Partially-finished-landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Button, Icon } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const gridStyle = { height: '100vh' };
+    const buttonStyle = { color: '#000000', width: '200px', background: '#006400', marginBottom: '20px' };
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={8}>
-            <h1>Study UHp Template</h1>
-          </Grid.Column>
-
-          <Grid.Column width={4}>
-            <Image disabled size='tiny' src="/images/studyuhp_logo_square.png"/>
-          </Grid.Column>
-
-        </Grid>
+        <div className='study-uhp-landing-background'>
+          <Grid style={gridStyle} verticalAlign='middle' textAlign='center' container>
+            <Grid.Column centered>
+              <Grid.Row>
+                <Image size='medium' src="/images/studyuhp_logo_square.png"/>
+              </Grid.Row>
+              <Grid.Row>
+                <Button animated style={buttonStyle} as={NavLink} exact to="/signup">
+                  <Button.Content visible>Sign Up</Button.Content>
+                  <Button.Content hidden> <Icon name='arrow right '/></Button.Content>
+                </Button>
+              </Grid.Row>
+              <Grid.Row/>
+              <Grid.Row>
+                <Button animated style={buttonStyle} as={NavLink} exact to="/signin">
+                  <Button.Content visible>Log In</Button.Content>
+                  <Button.Content hidden> <Icon name='arrow right '/></Button.Content>
+                </Button>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
