693a902e288b4685c018e88770a5439ccd6ad043
JohnHortonIV <jhorton3@hawaii.edu>
2020-04-16 17:02:48 -0700

    User home

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,18 +1,60 @@
 import React from 'react';
-import { Header, Grid } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { Grid, Table, Header, Loader, Button } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { StudySessions } from '../../api/studysessions/StudySessions';
+import StudySession from '../components/StudySession';
 
-/** Render a Not Found page if the user enters a URL that doesn't match any route. */
-export default class Dashboard extends React.Component {
-  render() {
-    return (
-        <Grid container centered className="main-content">
-          <Grid.Column>
-            <Header as="h2" textAlign="center"> User Dashboard</Header>
-            <Header as="h4" textAlign="center">
-              <p>Your upcoming sessions:</p>
-            </Header>
-          </Grid.Column>
-        </Grid>
-    );
-  }
+/** Renders a table containing all of the StudySessions. Use <StudySession> to render each row. */
+class Dashboard extends React.Component {
+
+    /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+    render() {
+        return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+    }
+
+    /** Render the page once subscriptions have been received. */
+    renderPage() {
+        return (
+            <Grid container centered className="main-content">
+                <Grid.Column>
+                    <Header as="h2" textAlign="center">Dashboard</Header>
+                    <Table celled inverted compact>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Course</Table.HeaderCell>
+                                <Table.HeaderCell>Topic</Table.HeaderCell>
+                                <Table.HeaderCell>Date</Table.HeaderCell>
+                                <Table.HeaderCell>Time</Table.HeaderCell>
+                                <Table.HeaderCell></Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {this.props.studysessions.map((studysession) => <StudySession
+                                key={studysession._id} studysession={studysession} />)}
+                        </Table.Body>
+                    </Table>
+                    <Button compact secondary as={NavLink} activeClassName="active" exact to="/add" key='add'>Create Session</Button>
+                </Grid.Column>
+            </Grid>
+        );
+    }
 }
+
+/** Require an array of StudySessions in the props. */
+Dashboard.propTypes = {
+    studysessions: PropTypes.array.isRequired,
+    ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+    // Get access to Sessions.
+    const subscription = Meteor.subscribe('StudySessions');
+    return {
+        studysessions: StudySessions.find({}).fetch(),
+        ready: subscription.ready(),
+    };
+})(Dashboard);
