fe25fb27460c149375232bcd9ca2b18899933349
Aaron Banks <abanks7@hawaii.edu>
2020-04-27 11:22:32 -1000

    added upcoming sessions to profile

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Card, Button, Loader, Image, Label, Segment, Grid, Header } from 'semantic-ui-react';
+import { Feed, Button, Loader, Image, Label, Segment, Grid, Header } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { UserProfiles } from '../../api/userprofiles/UserProfiles';
+import { StudySessions } from '../../api/studysessions/StudySessions';
+import SmallCalendar from '../components/SmallCalendar';
 
 /** A simple static component to render some text for the landing page. */
 class Profile extends React.Component {
@@ -46,9 +48,27 @@ class Profile extends React.Component {
                 {userBio}
               </Segment>
               <Segment inverted style={{ width: '25%' }}>
-                <span>Calendar</span>
-                <br/>
-                <span>Session Feed</span>
+                {/* <SmallCalendar/> */}
+                <Feed size='small'>
+                  <Header as='h4'>{this.props.userprofile[0].name.first}'s Upcoming Sessions</Header>
+                    {this.props.studysessions.map((studysession) => 
+                  <Feed.Event>
+                    <Feed.Label>
+                      <Label color='black' size='tiny'>
+                        {studysession.course}
+                      </Label>
+                    </Feed.Label>
+                    <Feed.Content>
+                    <Feed.Date>
+                      {studysession.date}
+                    </Feed.Date>
+                      <Feed.Summary>
+                        {studysession.topic}
+                      </Feed.Summary>
+                    </Feed.Content>
+                  </Feed.Event>
+                  )}
+                  </Feed>
               </Segment>
             </Segment.Group>
         </Grid.Column>
@@ -60,15 +80,18 @@ class Profile extends React.Component {
 /** Require an array of StudySessions in the props. */
 Profile.propTypes = {
   userprofile: PropTypes.array.isRequired,
+  studysessions: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Sessions.
-  const subscription = Meteor.subscribe('UserProfiles');
+  const subscription1 = Meteor.subscribe('UserProfiles');
+  const subscription2 = Meteor.subscribe('StudySessions');
   return {
+    studysessions: StudySessions.find({}).fetch(),
     userprofile: UserProfiles.find({}).fetch(),
-    ready: subscription.ready(),
+    ready: subscription1.ready() && subscription2.ready(),
   };
 })(Profile);
