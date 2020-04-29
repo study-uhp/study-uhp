2d99268318b286b69ebf8049fde34a02bc8c536c
micgainey <45333969+micgainey@users.noreply.github.com>
2020-04-29 01:53:57 -1000

    Revert "Update StudySessionAll.jsx"
    This reverts commit cefa5a74769efa9f66bdf3129490d63fdcbdebc7.

--- app/imports/ui/components/StudySessionAll.jsx
+++ app/imports/ui/components/StudySessionAll.jsx
@@ -1,36 +1,10 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Button, Table, Confirm } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { withTracker } from 'meteor/react-meteor-data';
-import { UserProfiles } from '../../api/userprofiles/UserProfiles';
-import { StudySessions } from '../../api/studysessions/StudySessions';
+import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Sessions table. See pages/ListStudySessions.jsx. */
 class StudySessionAll extends React.Component {
-  // state of Confirm.
-  state = { open: false, result: false }
-
-  // addUserToSession = () => this.props.studysession.participants.insert(this.props.userprofile._id)
-
-  // function that is called when join button is clicked.
-  show = () => this.setState({ open: true })
-
-  // if user hits yes
-  // = () => this.setState({ open: false, result: true })
-  handleConfirm(data) {
-    console.log(data);
-    // const { "participants": participants, _id } = data;
-    // const user = Meteor.user().user;
-    // StudySessions.update(_id,
-    //     { $push: { "participants": user } });
-    // this.setState({ open: false });
-  }
-
-  // if user hits no
-  handleCancel = () => this.setState({ open: false, result: false })
-
   render() {
     return (
         <Table.Row>
@@ -41,20 +15,13 @@ class StudySessionAll extends React.Component {
             {`${this.props.studysession.timeBegin} - ${this.props.studysession.timeEnd}`}
           </Table.Cell>
           <Table.Cell collapsing>
-            <Button onClick={this.show} secondary compact content='Join'/>
-            <Confirm
-                open={this.state.open}
-                cancelButton='Never mind'
-                confirmButton="Let's do it"
-                content='Would you like to join this session?'
-                onCancel={this.handleCancel}
-                onConfirm={this.handleConfirm}
-                size='mini'
-            />
+            <Link to={`/join/${this.props.studysession._id}`}>
+              <Button secondary compact content='Join' />
+            </Link>
           </Table.Cell>
           <Table.Cell collapsing>
             <Link to={`/viewstudysession/${this.props.studysession._id}`}>
-              <Button secondary compact content='View'/>
+              <Button secondary compact content='View' />
             </Link>
           </Table.Cell>
         </Table.Row>
@@ -65,20 +32,7 @@ class StudySessionAll extends React.Component {
 /** Require a document to be passed to this component. */
 StudySessionAll.propTypes = {
   studysession: PropTypes.object.isRequired,
-  userprofile: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-// export default withRouter(StudySessionAll);
-
-export default withTracker(() => {
-  // Get access to Sessions.
-  const subscription1 = Meteor.subscribe('UserProfiles');
-  const subscription2 = Meteor.subscribe('StudySessions');
-  return {
-    studysessions: StudySessions.find({}).fetch(),
-    // userprofile: UserProfiles.find({}).fetch(),
-    userprofile: UserProfiles.findOne({}),
-    ready: subscription1.ready() && subscription2.ready(),
-  };
-})(StudySessionAll);
+export default withRouter(StudySessionAll);
