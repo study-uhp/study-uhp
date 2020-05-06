c673d54118f744be9f61193eb4ec434d4635393f
micgainey <45333969+micgainey@users.noreply.github.com>
2020-05-05 22:14:03 -1000

    Finished issue 32!

--- app/imports/ui/pages/ViewStudySession.jsx
+++ app/imports/ui/pages/ViewStudySession.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Grid, Loader, Header } from 'semantic-ui-react';
+import { Grid, Loader, Header, Button } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
+import Swal from 'sweetalert2';
 import PropTypes from 'prop-types';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
 import { StudySessions } from '../../api/studysessions/StudySessions';
 import ViewSession from '../components/ViewSession';
+import { UserProfiles } from '../../api/userprofiles/UserProfiles';
 
 /** Renders the Page for editing a single document. */
 class ViewStudySession extends React.Component {
@@ -15,14 +17,40 @@ class ViewStudySession extends React.Component {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
+  handleClick(returnToClick) {
+    StudySessions.update(
+        { _id: returnToClick.doc._id },
+        { $addToSet: { participants: returnToClick.user.user } },
+        (error) => {
+          if (error) {
+            Swal.fire('Error', error.message, 'error');
+          } else {
+            Swal.fire('Success', 'Joined successfully', 'success');
+            console.log(returnToClick.doc.participants);
+          }
+        },
+    );
+  }
+
   /** Render the page and read ViewSession component */
   renderPage() {
+    const returnToClick = { doc: this.props.doc, user: this.props.user };
     return (
         <Grid container centered className="main-content">
-          <Grid.Column>
-
-            <ViewSession studysession={this.props.doc}/>
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column>
+              <ViewSession studysession={this.props.doc}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Button inverted color='green'
+                      onClick={() => this.handleClick(returnToClick)}
+                      content='Join Session'
+                      size='small'
+              />
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
     );
   }
@@ -31,6 +59,7 @@ class ViewStudySession extends React.Component {
 /** Require the presence of a Session in the props object. Uniforms adds 'model' to the props, which we use. */
 ViewStudySession.propTypes = {
   doc: PropTypes.object,
+  user: PropTypes.object,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -40,8 +69,10 @@ export default withTracker(({ match }) => {
   const documentId = match.params._id;
   // Get access to StudySessions.
   const subscription = Meteor.subscribe('ViewStudySession');
+  const subscription2 = Meteor.subscribe('UserProfiles');
   return {
     doc: StudySessions.findOne(documentId),
-    ready: subscription.ready(),
+    user: UserProfiles.findOne(),
+    ready: subscription.ready() && subscription2.ready(),
   };
 })(ViewStudySession);
